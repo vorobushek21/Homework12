63f698e559f77ac29246981e86b8fc994fa401af
Anton <anton.vorobev1999@mail.ru>
2023-01-13 18:19:18 +0300

    Выполнил второе дз по классам

--- src/Author.java
+++ src/Author.java
@@ -14,4 +14,23 @@ public class Author {
     public String getLastName() {
         return this.lastName;
     }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this.getClass() != object.getClass()) {
+            return false;
+        }
+        Author author = (Author) object;
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(firstName, lastName);
+    }
 }
